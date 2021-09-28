7656d8f8aaf67d0048de28bef934310fb5fb46c5
Vackyton AKA Pure Idiot Wants to die =1 <vackytonplays@gmail.com>
2021-09-28 09:04:21 -0400

    attempt 1 prob won't work

---
+++ attempt1.py
@@ -0,0 +1,15 @@
+import requests
+import threading
+import os
+
+os.system('pip install requests')
+#in case you don't have it
+gamepin = ""
+gamepin = int(gamepin)
+botnum = ""
+botnum = int(botnum)
+#number of bots and pins
+requests = request.requests("kahoot.it/" + gamepin)
+
+print(requests)
+#idk what will happen?
